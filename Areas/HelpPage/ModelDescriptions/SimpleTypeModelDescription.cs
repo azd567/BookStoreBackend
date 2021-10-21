f67151638721892cf98a321083a40def3e632e65
gmanjooran <gmanjooran@IND429>
2021-10-21 09:11:15 -0400

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookStoreBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookStoreBackend.WebApiApplication" Language="C#" %>
